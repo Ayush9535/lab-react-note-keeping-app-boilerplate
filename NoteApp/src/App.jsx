0db76f8abda5aebead17e26d30890088076e2a38
Ayush9535 <ghodkeayush22@gmail.com>
2024-01-09 21:41:55 +0530

    added

---
+++ NoteApp/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Editor from "./Components/editor"
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <Editor />
+      </div>
+    </>
+  )
+}
+
+export default App
